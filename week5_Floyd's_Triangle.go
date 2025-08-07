4ea57c83011dbf5c051a986da065ece58c9bb6df
mark spektor <markspektor8008@gmail.com>
2025-08-07 11:58:24 +0530

    Create week5_Floyd's_Triangle.go

---
+++ week5_Floyd's_Triangle.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	var rows int
+	fmt.Print("Enter the number of rows for Floyd's Triangle: ")
+	fmt.Scanln(&rows)
+	num := 1
+	fmt.Println("\nFloyd's Triangle:")
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d ", num)
+			num++
+		}
+		fmt.Println()
+	}
+} 
