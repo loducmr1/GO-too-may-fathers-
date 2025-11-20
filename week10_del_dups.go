11ebd3af7e895abc26c7a9442e824cd423e6996c
mark spektor <markspektor8008@gmail.com>
2025-11-20 08:26:07 +0530

    Update week10_del_dups.go

--- week10_del_dups.go
+++ week10_del_dups.go
@@ -1,20 +1,20 @@
 package main
 import "fmt"
-func removeDuplicates(s []string) []string {
-	bucket := make(map[string]bool)
-	var result []string
-	for _, str := range s {
-		if _, ok := bucket[str]; !ok {
-			bucket[str] = true
-			result = append(result, str)
+func removeDuplicates(arr []int) []int {
+	encountered := make(map[int]bool)
+	var result []int
+	for _, v := range arr {
+		if !encountered[v] {
+			encountered[v] = true
+			result = append(result, v)
 		}
 	}
 	return result
 }
 
 func main() {
-	array := []string{"abc", "cde", "efg", "efg", "abc", "cde"}
-	fmt.Println("The given array of string is:", array)
+	array := []int{1,2,3,2,4,5,1,6,3}
+	fmt.Println("The given array is:", array)
 	fmt.Println()
 	result := removeDuplicates(array)
 	fmt.Println("The array obtained after removing the duplicate entries is:", result)
